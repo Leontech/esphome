3fd392b275cdf1211cce4d33ee285069165f2725
Leontech <31631026+Leontech@users.noreply.github.com>
2025-08-05 10:50:58 +0200

    Update __init__.py

--- tfmini/__init__.py
+++ tfmini/__init__.py
@@ -1,24 +1,19 @@
-"""TFmini sensor external component."""
-from esphome.components import sensor
-from esphome.const import CONF_UART_ID
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.cpp_generator import Pvariable
-from esphome.cpp_types import App, sensor as sensor_ns, UARTDevice
+from esphome.const import CONF_ID
 
-# Definice C++ třídy senzoru
-TFminiSensor = sensor_ns.class_(
-    "TFminiSensor", App.polling_component, sensor.Sensor, UARTDevice
-)
+DEPENDENCIES = ['uart']
+AUTO_LOAD = ['sensor']
+CODEOWNERS = ["@Leontech"]  
 
-CONFIG_SCHEMA = sensor.SENSOR_PLATFORM_SCHEMA.extend(
-    {
-        cv.Required(CONF_UART_ID): cv.use_id(UARTDevice),
-    }
-).extend(cv.COMPONENT_SCHEMA.schema)
+
+tfmini_ns = cg.esphome_ns.namespace('tfmini')
+TFMiniComponent = tfmini_ns.class_('TFMiniComponent', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(TFMiniComponent)
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_UART_ID])
-    sens = Pvariable(config[CONF_ID], TFminiSensor(parent))
-    await sensor.register_sensor(sens, config)
-    await cg.register_component(sens, config)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
