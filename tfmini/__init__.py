c4962483d8fb5de7424fc4e22be381d5b920e6b1
Leontech <31631026+Leontech@users.noreply.github.com>
2025-08-05 10:04:32 +0200

    Create __init__.py

---
+++ tfmini/__init__.py
@@ -0,0 +1,24 @@
+"""TFmini sensor external component."""
+from esphome.components import sensor
+from esphome.const import CONF_UART_ID
+import esphome.config_validation as cv
+from esphome.core import CORE, coroutine_with_priority
+from esphome.cpp_generator import Pvariable, add, get_variable
+from esphome.cpp_types import App, Component, UARTDevice, sensor as sensor_ns
+
+# Definice C++ třídy senzoru
+TFminiSensor = sensor_ns.class_(
+    "TFminiSensor", App.polling_component, sensor.Sensor, UARTDevice
+)
+
+CONFIG_SCHEMA = sensor.SENSOR_PLATFORM_SCHEMA.extend(
+    {
+        cv.Required(CONF_UART_ID): cv.use_id(UARTDevice),
+    }
+).extend(cv.COMPONENT_SCHEMA.schema)
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_UART_ID])
+    sens = Pvariable(config[CONF_ID], TFminiSensor(parent))
+    await sensor.register_sensor(sens, config)
+    await cg.register_component(sens, config)
